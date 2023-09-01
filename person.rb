0ddc06fb6a5138e8f18919d219e3bf0f7c395e0d
tonnytech <tonnytei4@gmail.com>
2023-09-01 16:36:13 +0300

    remove unnecessary code from the repo

--- person.rb
+++ person.rb
@@ -1,6 +1,6 @@
 class Person
-  def initialize(id, age, name = 'Unknown', parent_permission: true)
-    @id = id
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -11,22 +11,13 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  # setters
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 
-  # -------------------
+  private
 
   def of_age?
     @age >= 18
   end
-
-  def can_use_services?
-    of_age? || @parent_permission
-  end
 end
-
-# person = Person.new(1,2,"Alice")
-# puts person.can_use_service?
-
-# person_2 = Teacher.new(1,5,"Tonny", false,"teaching")
-
-# puts person_2
